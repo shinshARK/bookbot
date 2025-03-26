60a8c49c6bf777d150aff70303e1728bab47a96a
shinshARK <137866950+shinshARK@users.noreply.github.com>
2025-03-26 20:54:26 +0700

    add character frequency count func

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import *
 
 def get_book_text(file_path):
     with open(file_path) as file:
@@ -9,6 +9,8 @@ def main():
     book_content = get_book_text("books/frankenstein.txt")
     word_count = count_words(book_content)
     print(f"{word_count} words found in the document")
+    char_count = count_character_frequency(book_content)
+    print(char_count)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,14 @@
 def count_words(text):
     return len(text.split())
+
+def count_character_frequency(text):
+    count = {}
+
+    for char in list(text):
+        char = char.lower()
+        if char not in count:
+            count[char] = 1
+        else:
+            count[char] += 1
+
+    return count
